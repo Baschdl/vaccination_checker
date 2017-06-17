968ae7ebba681fb69b7943d75a426e0850c99759
MarvinThiele <marvin.thiele@student.hpi.uni-potsdam.de>
2017-06-17 17:01:47 +0200

    Django

--- vaccination/vaccination_checker/views.py
+++ vaccination/vaccination_checker/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse("Hello, welcome to the Vaccination Checker.")
+    template = loader.get_template('templates\\vaccination_checker\\test.html')
+    return HttpResponse(template.render())
 
 def image_selector(request):
     return HttpResponse("Please upload your images.")
